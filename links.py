fb068b3a0c2c8e83fdf54dd0c5b5142c69c1d043
joker <pertsev.roman@gmail.com>
2016-02-16 14:42:53 +0300

    генератор адресов для локального файла

---
+++ links.py
@@ -0,0 +1,20 @@
+import os
+
+
+def main():
+    serv_name = input('Serv name [91.203.194.99]: ')
+    domain = input('Domain name: ')
+    zone = input('Zone [ru]: ')
+
+    if serv_name == '':
+        serv_name = '91.203.194.99'
+
+    if zone == '':
+        zone = 'ru'
+
+    dirs = input('Dirs path: ')
+
+    print('%s %s' % (serv_name, ' '.join(['%s.%s.%s' % (name, domain, zone) for name in os.listdir(dirs)])))
+
+if __name__ == '__main__':
+    main()
